f68ed054b54e5960b4faf407ba79837077d29af4
ryanmichael-nuera <100591152@alumnos.uc3m.es>
2026-09-10 20:33:11 -0500

    updated with part 2

---
+++ Project1_2.py
@@ -0,0 +1,17 @@
+# Script for Project 1 Part 2
+# ECE 4370: Engineering for Surgery
+# Fall 2026
+# Author:   Ryan Michael Nuera; ryan.michael.a.nuera@vanderbilt.edu
+#           EVERYONE ADD YOUR NAMES
+
+
+# PART A
+
+
+# PART B
+
+
+# PART C
+
+
+# PART D
